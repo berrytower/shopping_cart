b0df0652d9a62d8410025d62d1807f0296ada3bd
HAN899-KM <s259822431@gmail.com>
2023-11-27 19:34:09 +0800

    UI更新，目前仍有購物車顯示問題待修正

--- controllers/cart.php
+++ controllers/cart.php
@@ -2,39 +2,54 @@
 require_once('../models/CartModel.php');
 
 $cartModel = new CartModel($db);
+$action = $_GET['act'] ?? '';
 
-if ($_GET['act'] == 'add') {
-    
-    $cartItem = [
-        'product_id' => $_POST['productId'],
-        'quantity' => $_POST['quantity'],
-        'price' => $_POST['price'],
-    ];
+switch ($action) {
+    case 'add':
+        $productId = $_POST['productId'];
+        $quantity = $_POST['quantity'];
+        $price = $_POST['price'];
 
-    if ($cartItem['quantity'] <= 0) {
-        $response = [
-            'success' => false,
-            'message' => 'Quantity must be greater than 0'
-        ];
-    } else {
-        $cartItemId = $cartModel->addCartItem($cartItem['product_id'], $cartItem['quantity']);
-
-        if ($cartItemId) {
-            $response = [
-                'success' => true,
-                'message' => 'Cart item added successfully',
-                'cartItemId' => $cartItemId
-            ];
+        if ($quantity <= 0) {
+            $response = ['success' => false, 'message' => 'Quantity must be greater than 0'];
         } else {
-            $response = [
-                'success' => false,
-                'message' => 'Failed to add cart item'
-            ];
+            $cartItemId = $cartModel->addCartItem($productId, $quantity, $price);
+            if ($cartItemId) {
+                $response = ['success' => true, 'message' => 'Cart item added successfully', 'cartItemId' => $cartItemId];
+            } else {
+                $response = ['success' => false, 'message' => 'Failed to add cart item'];
+            }
         }
-    }
+        break;
 
-    header('Content-Type: application/json');
-    echo json_encode($response);
+    case 'list':
+        $cartItems = $cartModel->getCartItems();
+        $response = ['success' => true, 'cartItems' => $cartItems];
+        break;
+
+    case 'remove':
+        $cartItemId = $_POST['cartItemId'];
+        if ($cartModel->removeCartItem($cartItemId)) {
+            $response = ['success' => true, 'message' => 'Cart item removed successfully'];
+        } else {
+            $response = ['success' => false, 'message' => 'Failed to remove cart item'];
+        }
+        break;
+
+    case 'update':
+        $cartItemId = $_POST['cartItemId'];
+        $quantity = $_POST['quantity'];
+        if ($cartModel->updateCartItem($cartItemId, $quantity)) {
+            $response = ['success' => true, 'message' => 'Cart item updated successfully'];
+        } else {
+            $response = ['success' => false, 'message' => 'Failed to update cart item'];
+        }
+        break;
+
+    default:
+        $response = ['success' => false, 'message' => 'Invalid action'];
+        break;
 }
 
-
+header('Content-Type: application/json');
+echo json_encode($response);
--- models/CartModel.php
+++ models/CartModel.php
@@ -1,5 +1,6 @@
 <?php
 require('../dbconfig.php');
+
 class CartModel {
     private $db;
 
@@ -8,15 +9,36 @@ class CartModel {
     }
 
     public function addCartItem($productId, $quantity) {
-        $sql = "insert into shopping_cart (product_id, quantity) values (?, ?)";
+        $sql = "INSERT INTO shopping_cart (product_id, quantity) VALUES (?, ?)";
         $stmt = mysqli_prepare($this->db, $sql);
-
         mysqli_stmt_bind_param($stmt, 'ii', $productId, $quantity);
+
         if (mysqli_stmt_execute($stmt)) {
             return mysqli_insert_id($this->db);
         } else {
             return false;
         }
     }
-    // 缺: 刪除購物車商品、列出購物車商品
+
+    public function getCartItems() {
+        $sql = "SELECT sc.id, p.name, p.price, sc.quantity
+                FROM shopping_cart sc
+                JOIN products p ON sc.product_id = p.id";
+        $result = mysqli_query($this->db, $sql);
+        $cartItems = [];
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $cartItems[] = $row;
+        }
+
+        return $cartItems;
+    }
+
+    public function removeCartItem($cartItemId) {
+        $sql = "DELETE FROM shopping_cart WHERE id = ?";
+        $stmt = mysqli_prepare($this->db, $sql);
+        mysqli_stmt_bind_param($stmt, 'i', $cartItemId);
+
+        return mysqli_stmt_execute($stmt);
+    }
 }
