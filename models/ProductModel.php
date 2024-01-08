27bfe6fe224f110d4333297a7d25d0ceb021ae0b
berrytower <leegun047@gmail.com>
2024-01-08 12:44:28 +0800

    解決衝突

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -7,18 +7,13 @@ class ProductModel {
         $this->db = $db;
     }
 
-<<<<<<< HEAD
     public function getProducts($userId) {
         $sql = "SELECT * FROM products WHERE owner_ids = ?";
         $stmt = mysqli_prepare($this->db, $sql);
         mysqli_stmt_bind_param($stmt, 'i', $userId);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
-=======
-    public function getAllProducts() {
-        $sql = "select * from products";
-        $result = mysqli_query($this->db, $sql);
->>>>>>> 981d5bd520a5a67c4d08ac9bf5548e2a954aeaea
+
         $products = [];
         while ($row = mysqli_fetch_assoc($result)) {
             $products[] = $row;
@@ -29,11 +24,7 @@ class ProductModel {
     public function addProduct($name, $description, $price) {
         
 
-<<<<<<< HEAD
         $sql = "INSERT INTO products (name, description, price, owner_id) VALUES (?, ?, ?, ?)";
-=======
-        $sql = "insert into products (name, description, price) values (?, ?, ?)";
->>>>>>> 981d5bd520a5a67c4d08ac9bf5548e2a954aeaea
         $stmt = mysqli_prepare($this->db, $sql);
         mysqli_stmt_bind_param($stmt, 'ssd', $name, $description, $price);
         if (mysqli_stmt_execute($stmt)) {
